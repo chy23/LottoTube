d2b63eb991cf78d096d1c6b4a55aa460959dfb95
Chiahua <chiahua@Chiahua-MacBook-Pro-M1-2.local>
2026-08-05 12:57:22 +0800

    Feat: Add hidden creator credits to HTML and JS console

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,9 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+// 網站建立自楊家驊老師 The website was created by Teacher ChiahuaYang
+console.log("%c網站建立自楊家驊老師 The website was created by Teacher ChiahuaYang", "color: #007AFF; font-size: 14px; font-weight: bold;");
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
